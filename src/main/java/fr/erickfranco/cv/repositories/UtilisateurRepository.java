f54adbcbebe9a2880c75f65b3ab9b935aebd54a0
Erick <erickfrancodelgado@hotmail.com>
2021-03-29 15:08:39 +0200

    ajout d'une methode UtilisateurRepository

--- src/main/java/fr/erickfranco/cv/repositories/UtilisateurRepository.java
+++ src/main/java/fr/erickfranco/cv/repositories/UtilisateurRepository.java
@@ -2,11 +2,18 @@ package fr.erickfranco.cv.repositories;
 
 import fr.erickfranco.cv.models.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 /**
  * @author Erick Franco
  */
 @Repository
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
+
+    @Query(" select u from Utilisateur u " +
+            " where u.nom = ?1")
+    Optional<Utilisateur> findUserWithName(String username);
 }
