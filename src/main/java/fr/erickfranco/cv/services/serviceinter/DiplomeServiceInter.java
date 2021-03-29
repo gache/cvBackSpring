281e8b4a6319908033f12a0adf03451d6de3f3ed
Erick <erickfrancodelgado@hotmail.com>
2021-03-29 15:18:14 +0200

    ajout interface DiplomeServiceInter

---
+++ src/main/java/fr/erickfranco/cv/services/serviceinter/DiplomeServiceInter.java
@@ -0,0 +1,22 @@
+package fr.erickfranco.cv.services.serviceinter;
+
+import fr.erickfranco.cv.models.Diplome;
+
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Erick Franco
+ */
+
+public interface DiplomeServiceInter {
+
+    List<Diplome> findAllDiplome();
+
+    Diplome saveDiplome(Diplome diplome);
+
+    Optional<Diplome> findDiplomeById(Integer id);
+
+    void deleteDiplomeById(Integer id);
+}
