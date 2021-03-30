8889417c4cef5ab9f80cefb7b9610491363f02bc
gache <erickfrancodelgado@hotmail.com>
2021-03-30 13:55:20 +0200

    incrementation dans la class LangageController la methode createLangage

--- src/main/java/fr/erickfranco/cv/controllers/LangageController.java
+++ src/main/java/fr/erickfranco/cv/controllers/LangageController.java
@@ -4,10 +4,7 @@ import fr.erickfranco.cv.models.Langage;
 import fr.erickfranco.cv.services.serviceinter.LangageServiceInter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -33,6 +30,11 @@ public class LangageController {
     @GetMapping("/langageId/{id}")
     public ResponseEntity<Optional<Langage>> getLangageById(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(langageServiceInter.findLangageById(id));
+    }
+
+    @PostMapping("/createLangage")
+    public ResponseEntity<Langage> createLangage(@RequestBody Langage langage) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(langageServiceInter.saveLangage(langage));
     }
 
 
