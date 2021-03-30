d4946292ad3a55d963e8a983642b8fa40424637e
gache <erickfrancodelgado@hotmail.com>
2021-03-30 14:13:28 +0200

    incrementation dans la class ProjetController la methode projetById

--- src/main/java/fr/erickfranco/cv/controllers/ProjetController.java
+++ src/main/java/fr/erickfranco/cv/controllers/ProjetController.java
@@ -5,10 +5,12 @@ import fr.erickfranco.cv.services.serviceinter.ProjetServiceInter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Erick Franco
@@ -26,6 +28,11 @@ public class ProjetController {
     @GetMapping("/projets")
     public ResponseEntity<List<Projet>> getProjets() {
         return ResponseEntity.status(HttpStatus.OK).body(projetServiceInter.findAllProjet());
+    }
+
+    @GetMapping("projetsId/{id}")
+    public ResponseEntity<Optional<Projet>> projetById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(projetServiceInter.findProjetById(id));
     }
 
 }
