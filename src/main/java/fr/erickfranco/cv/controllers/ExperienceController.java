8c8bbf131f337fbf52cd6f41d266a4e37630659a
gache <erickfrancodelgado@hotmail.com>
2021-03-30 13:43:14 +0200

    incrementation dans la class ExperienceServiceInter la methode experienceById

--- src/main/java/fr/erickfranco/cv/controllers/ExperienceController.java
+++ src/main/java/fr/erickfranco/cv/controllers/ExperienceController.java
@@ -5,10 +5,12 @@ import fr.erickfranco.cv.services.serviceinter.ExperienceServiceInter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Erick Franco
@@ -27,6 +29,13 @@ public class ExperienceController {
     public ResponseEntity<List<Experience>> getDiplomes() {
         return ResponseEntity.status(HttpStatus.OK).body(experienceServiceInter.findAll());
     }
+
+    @GetMapping("/experienceId/{id}")
+    public ResponseEntity<Optional<Experience>> experienceById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(experienceServiceInter.findById(id));
+    }
+
+
 
 
 
