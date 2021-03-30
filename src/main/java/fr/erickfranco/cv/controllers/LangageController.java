5e88614150ccb93d3c0c9ee78996490fafe321f8
gache <erickfrancodelgado@hotmail.com>
2021-03-30 13:54:32 +0200

    incrementation dans la class LangageController la methode getLangageById

--- src/main/java/fr/erickfranco/cv/controllers/LangageController.java
+++ src/main/java/fr/erickfranco/cv/controllers/LangageController.java
@@ -5,10 +5,12 @@ import fr.erickfranco.cv.services.serviceinter.LangageServiceInter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Erick Franco
@@ -26,6 +28,11 @@ public class LangageController {
     @GetMapping("/langages")
     public ResponseEntity<List<Langage>> getLangages() {
         return ResponseEntity.status(HttpStatus.OK).body(langageServiceInter.findAllLangage());
+    }
+
+    @GetMapping("/langageId/{id}")
+    public ResponseEntity<Optional<Langage>> getLangageById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(langageServiceInter.findLangageById(id));
     }
 
 
