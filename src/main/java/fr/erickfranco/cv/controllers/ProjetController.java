4ee05891a29b3264d92bbeb3cb75a3eb5993bd8c
gache <erickfrancodelgado@hotmail.com>
2021-03-30 14:14:20 +0200

    incrementation dans la class ProjetController la methode createProjet

--- src/main/java/fr/erickfranco/cv/controllers/ProjetController.java
+++ src/main/java/fr/erickfranco/cv/controllers/ProjetController.java
@@ -4,10 +4,7 @@ import fr.erickfranco.cv.models.Projet;
 import fr.erickfranco.cv.services.serviceinter.ProjetServiceInter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -33,6 +30,11 @@ public class ProjetController {
     @GetMapping("projetsId/{id}")
     public ResponseEntity<Optional<Projet>> projetById(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(projetServiceInter.findProjetById(id));
+    }
+
+    @PostMapping("/createProjet")
+    public ResponseEntity<Projet> createProjet(@RequestBody Projet projet) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(projetServiceInter.saveProjet(projet));
     }
 
 }
