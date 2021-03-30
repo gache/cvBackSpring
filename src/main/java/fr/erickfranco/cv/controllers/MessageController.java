f741d1d9699f5284f1d2d40a4d78aae4b7a5e8fb
gache <erickfrancodelgado@hotmail.com>
2021-03-30 14:04:35 +0200

    incrementation dans la class MessageController la methode createMessage

--- src/main/java/fr/erickfranco/cv/controllers/MessageController.java
+++ src/main/java/fr/erickfranco/cv/controllers/MessageController.java
@@ -4,10 +4,7 @@ import fr.erickfranco.cv.models.Message;
 import fr.erickfranco.cv.services.serviceinter.MessageServiceInter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -36,5 +33,9 @@ public class MessageController {
         return ResponseEntity.status(HttpStatus.OK).body(messageServiceInter.findMessageById(id));
     }
 
+    @PostMapping("/createMessage")
+    public ResponseEntity<Message> createMessage(@RequestBody Message message) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(messageServiceInter.saveMessage(message));
+    }
 
 }
