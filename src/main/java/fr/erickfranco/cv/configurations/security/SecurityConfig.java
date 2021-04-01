51cd7bd149269af93d79cc1af11633f0219e306c
Erick <erickfrancodelgado@hotmail.com>
2021-04-01 21:23:28 +0200

    incrementation class SecurityConfig

--- src/main/java/fr/erickfranco/cv/configurations/security/SecurityConfig.java
+++ src/main/java/fr/erickfranco/cv/configurations/security/SecurityConfig.java
@@ -3,7 +3,10 @@ package fr.erickfranco.cv.configurations.security;
 import fr.erickfranco.cv.services.serviceinter.UtilisateurServiceInter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -48,6 +51,45 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception{
+        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**")
+                .antMatchers("/swagger-ui.html")
+                .antMatchers("/test/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().and().csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(problemSupport)  // comment this line, display form from spring sec
+                .accessDeniedHandler(problemSupport)
+                .and()
+                .formLogin()
+                .loginProcessingUrl("/api/authentication")
+                .successHandler(ajaxAuthenticationSuccessHandler())
+                .failureHandler(ajaxAuthenticationFailureHandler())
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/api/logout")// will return to login page afterward
+                .logoutSuccessHandler(ajaxLogoutSuccessHandler())
+                .deleteCookies("JSESSIONID")
+                .invalidateHttpSession(true)
+                .permitAll()
+                .and()
+                .headers()
+                .frameOptions()
+                .disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/account").permitAll()
+                .antMatchers("/authenticate").permitAll()
+                .antMatchers("/authentication").permitAll()
+                .antMatchers("/**").authenticated();
     }
 
 
