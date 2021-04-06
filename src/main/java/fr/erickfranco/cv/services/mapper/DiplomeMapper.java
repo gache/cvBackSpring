1ed21a4948b9b3ffb1dd9d6bf6d70ad901fcc077
gache <erickfrancodelgado@hotmail.com>
2021-04-06 20:54:20 +0200

    ajout de la class DiplomeMapper dans dto

---
+++ src/main/java/fr/erickfranco/cv/services/mapper/DiplomeMapper.java
@@ -0,0 +1,23 @@
+package fr.erickfranco.cv.services.mapper;
+
+import fr.erickfranco.cv.models.Diplome;
+import fr.erickfranco.cv.services.DTO.DiplomeDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface DiplomeMapper extends EntityMapper<DiplomeDTO, Diplome> {
+
+    DiplomeDTO toDTO(Diplome entity);
+
+    Diplome toEntity(DiplomeDTO dto);
+
+    default Diplome fromId(Long id) {
+        if (id == null) {
+            return null;
+        }
+        Diplome typePaiement = new Diplome();
+        typePaiement.setId(id);
+        return typePaiement;
+    }
+
+}
